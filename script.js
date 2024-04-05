22f8589b6e0d9d28a3d6e5b69908c25c6cc3a5df
H3kk4 <78410927+H3kk4@users.noreply.github.com>
2024-04-05 16:28:53 +0200

    Formation manque carousels + background

--- script.js
+++ script.js
@@ -5,23 +5,65 @@ menuBtn.addEventListener('click', () => {
 })
 
 
-function openPDF() {
+function openPDF(doc) {
+    const embed = document.querySelector("#embed")
     const overlay = document.querySelector("#overlay")
     const popup = document.querySelector(".popup")
+
+    var embedInit = embed.src
+
+    embed.src += doc + ".pdf"
 
     overlay.style.display = "block"
     popup.style.display = "block"
+    
 
     overlay.addEventListener('click', closePDF)
+
+    function closePDF() {
+        
+    
+        const overlay = document.querySelector("#overlay")
+        const popup = document.querySelector(".popup")
+    
+        overlay.style.display = "none"
+        popup.style.display = "none"
+
+        
+    
+        embed.src = embedInit
+        overlay.removeEventListener('click', closePDF)
+        
+    }
 }
 
-function closePDF() {
+function openCarousel(id) {
     const overlay = document.querySelector("#overlay")
-    const popup = document.querySelector(".popup")
+    const popup = document.querySelector(".popup-" + id)
+    const embed = document.querySelector(".carousel-item")
 
-    overlay.style.display = "none"
-    popup.style.display = "none"
 
-    overlay.removeEventListener('click', closePDF)
+    overlay.style.display = "block"
+    popup.style.display = "block"
+    
+    embed.addEventListener('click', openFS(embed.id))
+    overlay.addEventListener('click', closeCarousel)
 
+    function openFS(id){
+        console.log(id)
+    }
+
+
+    function closeCarousel() {
+        const overlay = document.querySelector("#overlay")
+        const popup = document.querySelector(".popup-" + id)
+    
+        overlay.style.display = "none"
+        popup.style.display = "none"
+
+        overlay.removeEventListener('click', closeCarousel)
+        
+    }
 }
+
+
